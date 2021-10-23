d4cd9950578a462b701f8315448a09122f867432
Artem Prilypchan <trekwsp@gmail.com>
2021-10-23 15:58:07 +0300

    test

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int rand() {
+    List<int> values = List.generate(256, (_) => _);
+    values.shuffle();
+    return values[0];
+  }
+
+  void onTap() {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: onTap,
+        child: AnimatedContainer(
+          color: Color.fromRGBO(rand(), rand(), rand(), 1),
+          duration: const Duration(milliseconds: 200),
+          child: const Center(
+            child: Text(
+              'Hey there',
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
